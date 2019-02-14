40057c56b2ed566c5e742e2d78d4b00248532294
Nicholas Romano <njrom@Nicholass-MacBook-Pro-15.local>
2019-02-14 10:56:36 -0500

    Added alpha-beta pruning to Minimax algorithm

--- src/AI_Minimax.java
+++ src/AI_Minimax.java
@@ -2,29 +2,38 @@ import java.util.ArrayList;
 
 
 public class AI_Minimax implements AI{
-    public int depth = 1;
+    /*
+    AI_Minimax uses alpha beta pruning on a heuristic minimax algorithm
+     */
+    static int depthLimit = 1;
+    static int depth = 0;
     @Override
     public int decideTurn(Board board) {
-
+        
         ArrayList<Integer> pActions = board.possibleActions();
         int maxCol = 0;
         int maxValue = Integer.MIN_VALUE;
+        int alpha = Integer.MIN_VALUE;
+        int beta = Integer.MAX_VALUE;
+
         for(int i = 0; i < pActions.size(); i++) {
             Board cb = Board.copyBoard(board);
             cb = Board.dropChip(cb, pActions.get(i));
             // System.out.printf("Move on %s \n", pActions.get(i));
-            if(maxValue <= minimax(cb, depth, false)) {
+            if(maxValue <= minimax(cb, 5, alpha, beta, false)) {
                 // System.out.printf("Max Col now %s \n", pActions.get(i));
-                maxValue = minimax(cb, depth, false); // Bug, if it isn't odd depth it will be the wrong choice
-
+                maxValue = minimax(cb, 5, alpha, beta, false); // Bug, if it isn't odd depth it will be the wrong choice
                 maxCol = pActions.get(i);
+                if(alpha >= beta) {
+                    break;
+                }
             }
         }
 
         return maxCol;
     }
 
-    public int minimax(Board board, int depth, boolean maxPlayer) {
+    public int minimax(Board board, int depth, int alpha, int beta, boolean maxPlayer) {
         if( depth == 0 || board.isTerminal()) {
             // GameIO.printBoard(board);
             if(maxPlayer) {
@@ -42,7 +51,11 @@ public class AI_Minimax implements AI{
                Board cb = Board.copyBoard(board);
                cb = Board.dropChip(cb, pactions.get(i));
 
-               val = Math.max(val, minimax(cb, depth -1, false));
+               val = Math.max(val, minimax(cb, depth -1, alpha, beta, false));
+               alpha = Math.max(alpha, val);
+               if(alpha >= beta) {
+                   break;
+               }
            }
            return val;
 
@@ -53,7 +66,11 @@ public class AI_Minimax implements AI{
                 Board cb = Board.copyBoard(board);
                 cb = Board.dropChip(cb, pactions.get(i));
 
-                val = Math.min(val, minimax(cb, depth -1, true));
+                val = Math.min(val, minimax(cb, depth -1, alpha, beta, true));
+                beta = Math.min(beta, val);
+                if(alpha >= beta) {
+                    break;
+                }
             }
             return val;
 
